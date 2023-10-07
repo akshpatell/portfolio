301b6585e44c468406ed13d5c3a76abfbbeaff9d
aksh <akshpatel08032003@gmail.com>
2023-10-07 23:08:50 +0530

    commit code

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "@/components/Header";
+import Male from "@/components/Male";
 
 const page = () => {
   return (
     <div>
       <Header />
+      <Male />
     </div>
   );
 };
---
+++ src/components/Male/index.jsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import React from "react";
+
+const Male = () => {
+  return (
+    <div className=" flex justify-center items-center p-5 bg-emerald-800 gap-5 flex-wrap-reverse">
+      <div>
+        <h1 className="text-5xl max-sm:text-4xl font-semiboldbold text-white">
+          Software Developer:
+        </h1>
+        <h1 className="text-5xl max-sm:text-4xl font-semiboldbold text-white">
+          Transforming Ideas into
+        </h1>
+        <h1 className="text-5xl max-sm:text-4xl font-semibold text-red-500">
+          Digital Reality
+        </h1>
+        <p className="text-white font-thin text-lg max-sm:text-base">
+          I am aksh patel, you can see my portfolio from bottom of the website.
+          click on the button to contact me
+        </p>
+      </div>
+      <div className="bg-emerald-600 rounded-lg">
+        <Image src="/images/person.png" width={300} height={300} alt="alt" />
+      </div>
+    </div>
+  );
+};
+
+export default Male;
