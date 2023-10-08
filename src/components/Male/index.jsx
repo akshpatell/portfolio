471d4879e0c77695e596ad2d1060831f7d180357
aksh <akshpatel08032003@gmail.com>
2023-10-08 12:37:28 +0530

    added two buttons in male components

--- src/components/Male/index.jsx
+++ src/components/Male/index.jsx
@@ -1,23 +1,38 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Male = () => {
   return (
     <div className=" flex justify-center items-center p-5 bg-emerald-800 gap-5 flex-wrap-reverse w-full">
-      <div>
-        <h1 className="text-5xl max-sm:text-4xl font-semiboldbold text-white">
-          Software Developer:
-        </h1>
-        <h1 className="text-5xl max-sm:text-4xl font-semiboldbold text-white">
-          Transforming Ideas into
-        </h1>
-        <h1 className="text-5xl max-sm:text-4xl font-semibold text-red-500">
-          Digital Reality
-        </h1>
-        <p className="text-white font-thin text-lg max-sm:text-base">
-          I am aksh patel, you can see my portfolio from bottom of the website.
-          click on the button to contact me
-        </p>
+      <div className="flex flex-col gap-3">
+        <div>
+          <h1 className="text-5xl max-sm:text-4xl font-semiboldbold text-white">
+            Software Developer:
+          </h1>
+          <h1 className="text-5xl max-sm:text-4xl font-semiboldbold text-white">
+            Transforming Ideas into
+          </h1>
+          <h1 className="text-5xl max-sm:text-4xl font-semibold text-red-500">
+            Digital Reality
+          </h1>
+          <p className="text-white font-thin text-lg max-sm:text-base">
+            I am aksh patel, you can see my portfolio from bottom of the
+            website. click on the button to contact me
+          </p>
+        </div>
+        <div className="flex gap-3">
+          <Link href="/contact-me">
+            <button className="text-white bg-red-500 py-2 px-5 rounded-full hover:bg-red-600 ">
+              Contact me
+            </button>
+          </Link>
+          <Link href="/products">
+            <button className="text-black bg-white py-2 px-5 rounded-full hover:bg-gray-300 ">
+              Products
+            </button>
+          </Link>
+        </div>
       </div>
       <div>
         <Image
