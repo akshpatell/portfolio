b90f34621020e3c49aca42ca4f7b35b3046f94a2
aksh <akshpatel08032003@gmail.com>
2023-10-12 20:26:34 +0530

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "@/components/Header";
-import Male from "@/components/Male";
+import Header from "../components/Header";
+import Male from "../components/Male";
 
 const page = () => {
   return (
